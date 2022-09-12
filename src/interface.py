b49a06ac3fc3e0c09a2ee26e5ddd6150b86b3330
landon1523 <62727646+landon1523@users.noreply.github.com>
2022-09-11 19:49:16 -0500

    Adding docs folder, compartmentalizing source code into src folder.

--- src/interface.py
+++ src/interface.py
@@ -1,4 +1,3 @@
-import mysqlManager
 import customerManager
 import masseuseManager
 import appointmentManager
@@ -8,30 +7,30 @@ import availabilityManager
 """
 
 # Gather info from user to create a new customer
-customerManager.insert_customer(customerId,name,address,email,phone)
+customerManager.insert_customer(customerId, name, address, email, phone)
 
 # Gather info from user to create a new masseuse
-masseuseManager.insert_masseuse(masseuseId,name,address,email,phone)
+masseuseManager.insert_masseuse(masseuseId, name, address, email, phone)
 
 # Gather info from user About a masseuse
 # Note: 1=Sunday, 2=Monday, 3=Tuesday, 4=Wednesday, 5=Thursday, 6=Friday, 7=Saturday.
 # Go through each week day (int, 2 – 6), 
 # ask for start time (int, 0 – 23), end time (int, 0 – 23), or skip day of the week
 # end time > start time
-availabilityManager.insert_availability(masseuseId,dow,start,end)
+availabilityManager.insert_availability(masseuseId, dow, start, end)
 
 # Populate appointments let's say 8 weeks out
 # 8 start times should be created for each day (9a to 5p)
 # each start time should have 3 rooms
 # this creates empty appointments with status OPEN
 # insert_appointment(start_time,room,status,masseuseId,customerId)
-appointmentManager.insert_appointment(start_time,room,"OPEN",NULL,NULL)
+appointmentManager.insert_appointment(start_time, room, "OPEN", NULL, NULL)
 
 # Use to book or cancel appointment
 # update_appointment(appointmentId,status,masseuseId,customerId)
-appointmentManager.update_appointment(appointmentId,"BOOKED",masseuseId,customerId)
+appointmentManager.update_appointment(appointmentId, "BOOKED", masseuseId, customerId)
     # Cancel APPT
-appointmentManager.update_appointment(appointmentId,"OPEN",NULL,NULL)
+appointmentManager.update_appointment(appointmentId, "OPEN", NULL, NULL)
 
 # ! This might logically be the start (when you open the program)
 # ask customer which day they want to book/cancel
