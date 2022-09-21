e6bbb05fe82404928d9aef9a6289ad2f6e5415b7
JamesChapmanNV <89288003+JamesChapmanNV@users.noreply.github.com>
2022-09-21 15:43:21 -0500

    Revert "Created database, tables. Everything is empty. I used my SQL to create diagrams"
    This reverts commit 01cdd051085f94f690f2eb508be6aadf88cd6083.

--- config.py
+++
@@ -1,4 +0,0 @@
-host = 'massage-company.cqkj5druob3t.us-east-1.rds.amazonaws.com'
-user = 'admin'
-passwd = 'refmas-wAmho5-myvkys'
-db_name = 'massageCompany'
--- src/mysqlManager.py
+++ src/mysqlManager.py
@@ -11,33 +11,37 @@ try:
     cnx = mysql.connector.connect(
             host = config.host,
             user = config.user,
-            password = config.passwd,
-            database = config.db_name
-            )
+            password = config.passwd)
     print(cnx)
     cursor = cnx.cursor()
 except:
     print("connection error")
-
-    
-#creates db and tables, only need to run once
 """
-try:
-    cursor.execute("USE {}".format(config.db_name))
-except mysql.connector.Error as err:
-    print("Database {} does not exists.".format(config.db_name))
-    if err.errno == errorcode.ER_BAD_DB_ERROR:
-        create_database(cursor, config.db_name)
-        print("Database {} created successfully.".format(config.db_name))
-        cnx.database = config.db_name
-    else:
-        print(err)
+.......................................................................................
+.......................................................................................
+"""
+
+#creates db
+def create_database(cursor, database):
+    try:
+        cursor.execute(
+            "CREATE DATABASE {} DEFAULT CHARACTER SET 'utf8'".format(database))
+    except mysql.connector.Error as err:
+        print("Failed creating database: {}".format(err))
         exit(1)
-create_tables(cursor)
-.......................................................................................
-.......................................................................................
-"""
 
+""" needed?"""
+# try:
+#     cursor.execute("USE {}".format(config.db_name))
+# except mysql.connector.Error as err:
+#     print("Database {} does not exists.".format(config.db_name))
+#     if err.errno == errorcode.ER_BAD_DB_ERROR:
+#         #create_database(cursor, db_name)
+#         print("Database {} created successfully.".format(config.db_name))
+#         cnx.database = config.db_name
+#     else:
+#         print(err)
+#         exit(1)
 
 def create_database(cursor, database):
     try:
@@ -95,4 +99,5 @@ TABLES['masseuseavailability'] = (
     " start int NOT NULL,"
     " end int NOT NULL"
     ")")
+
 
