0bbdd301d8f67d1a459c28f8de2fa6bc4c3a984c
Dan Ring <dmring2@mail.fhsu.edu>
2022-12-07 13:02:04 -0600

    merging attempt fix 2

--- src/user_interface/frames/management_frames/manageAppointmentFrame.py
+++ src/user_interface/frames/management_frames/manageAppointmentFrame.py
@@ -21,21 +21,36 @@ class ManageApptFrame(tk.Toplevel):
         self.__inner_frame = tk.Frame(self, bd=2, bg='#CCCCCC', relief=tk.SOLID, padx=10, pady=10)
 
         tk.Label(self.__inner_frame,
-                 text="Date",
+                 text="Appointment ID",
                  bg='#CCCCCC',
-                 font=f).grid(row=0, column=0, sticky=tk.W, pady=10)
+                 font=f).grid(row=0, column=0, pady=10)
         tk.Label(self.__inner_frame,
-                 text="Time",
+                 text="123",
+                 bg='#CCCCCC',
+                 font=f).grid(row=0, column=1, pady=10)
+        tk.Label(self.__inner_frame,
+                 text="Date",
                  bg='#CCCCCC',
                  font=f).grid(row=1, column=0, pady=10)
         tk.Label(self.__inner_frame,
-                 text="Masseuse",
+                 text="Time",
                  bg='#CCCCCC',
                  font=f).grid(row=2, column=0, pady=10)
         tk.Label(self.__inner_frame,
-                 text="Customer",
+                 text="Masseuse",
                  bg='#CCCCCC',
                  font=f).grid(row=3, column=0, pady=10)
+        tk.Label(self.__inner_frame,
+                 text="Customer",
+                 bg='#CCCCCC',
+                 font=f).grid(row=4, column=0, pady=10)
+        tk.Label(self.__inner_frame,
+                 text="Room #",
+                 bg='#CCCCCC',
+                 font=f).grid(row=5, column=0, pady=10)
+
+        # Get appointmentID if available
+
 
         # Input values
         self.__date_var = tk.StringVar()
@@ -46,13 +61,28 @@ class ManageApptFrame(tk.Toplevel):
         # Set callback
         self.__date_var.trace_add('write', callback=self.my_callback)
 
+        # self.date_input = tk.Entry(self.__inner_frame, textvariable=self.__date_var, font=f)
         self.date_input = appointments.Appointments(self.__inner_frame, self.__date_var)
+        '''
+        self.show_id = tk.StringVar(self, value="123")
+        This is probably not necessary. Trying to figure out how to update appointmentID correctly.
+        '''
+
+        rooms = ["1", "2", "3", "4", ]
+        room_select = tk.StringVar()
+        room_select.set(rooms[0])
+
+        # self.time_input = tk.Entry(self.__inner_frame, font=f)
+
+        # current_time = tk.StringVar()
+        # self.time_input = tk.OptionMenu(self.__inner_frame, self.__time_var, *self.get_times())
 
         masseuse_choices = masseuseManager.get_all_masseuse_names()
         self.masseuse_input = tk.OptionMenu(self.__inner_frame, self.__masseuse_var, *masseuse_choices)
 
         customer_choices = customerManager.get_all_customer_names()
         self.customer_input = tk.OptionMenu(self.__inner_frame, self.__customer_var, *customer_choices)
+        self.room_num = tk.OptionMenu(self.__inner_frame, room_select, *rooms)
         save_button = tk.Button(self.__inner_frame,
                                 width=15,
                                 text='Save',
@@ -61,11 +91,25 @@ class ManageApptFrame(tk.Toplevel):
                                 cursor='hand2',
                                 command=self.__save_appointment)
 
-        self.date_input.grid(row=0, column=1, pady=10, padx=20)
-        self.time_input.grid(row=1, column=1, pady=10, padx=20)
-        self.masseuse_input.grid(row=2, column=1, pady=10, padx=20)
-        self.customer_input.grid(row=3, column=1, pady=10, padx=20)
-        save_button.grid(row=4, column=1, pady=10, padx=20)
+        delete_button = tk.Button(self.__inner_frame,
+                                  width=15,
+                                  text='Delete',
+                                  font=f,
+                                  relief=tk.SOLID,
+                                  cursor='hand2',
+                                  command=self.__delete_appointment)
+        '''
+        self.show_id.grid(row=0, column=1, pady=10, padx=20)
+        Trying to get appointmentID to display correctly
+        '''
+        self.date_input.grid(row=1, column=1, pady=10, padx=20)
+        self.time_input.grid(row=2, column=1, pady=10, padx=20)
+        self.masseuse_input.grid(row=3, column=1, pady=10, padx=20)
+        self.customer_input.grid(row=4, column=1, pady=10, padx=20)
+        self.room_num.grid(row=5, column=1, pady=10, padx=20)
+        save_button.grid(row=6, column=0, pady=10, padx=15)
+        delete_button.grid(row=6, column=1, pady=10, padx=15)
+        # self.date_input.bind("<<CalendarSelected>>", lambda: print(self.date_input.selection_get()))
 
         self.__inner_frame.pack()
 
@@ -78,19 +122,27 @@ class ManageApptFrame(tk.Toplevel):
     def __save_appointment(self):
         print(self.__date_var.get(), self.__time_var.get(), self.__masseuse_var.get())
         time1 = datetime.strptime(self.__time_var.get(), '%I:%M %p').strftime('%H')
-        dt = datetime.strptime(self.__date_var.get(),'%m/%d/%y') + timedelta(hours = int(time1))
+        dt = datetime.strptime(self.__date_var.get(), '%m/%d/%y') + timedelta(hours=int(time1))
         appointmentManager.insert_appointment(f'{dt}', 1, 'BOOKED', 61789, 10325)
         self.main_frame.make_appointment_scheduler()
+
+    def __delete_appointment(self):
+        appointmentManager.delete_appointment(902)
+        self.main_frame.make_appointment_scheduler()
+
 
     def my_callback(self, var, index, mode):
         print("Traced variable {}".format(self.__date_var.get()))
         self.time_input = tk.OptionMenu(self.__inner_frame, self.__time_var, *self.get_times())
 
+        '''if self.__date_var.get() != "10/30/22":
+            self.__time_var.set("Test")'''
+
     def get_times(self):
         seletedDate = self.__date_var.get()
-        if seletedDate == '11/6/22':
+        if (seletedDate == '11/6/22'):
             return [1, 2, 3]
-        if seletedDate == '':
+        if (seletedDate == ''):
             return [
                 '9:00 AM',
                 '10:00 AM',
@@ -124,7 +176,7 @@ class ManageApptFrame(tk.Toplevel):
         # count number of appointments at that time scheduled
         # because there are 3 rooms, if <3, there is an appointment available
         for time in times_dict:
-            if times_dict[time] < 3:
+            if (times_dict[time] < 3):
                 times.append(time)
 
         print(times)
